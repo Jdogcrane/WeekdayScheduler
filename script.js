f82d71b95bf84c4f34915e4f5e72392572dc7221
Josh <jdogcrane@gmail.com>
2021-05-24 16:30:55 -0400

    simplified if statements

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-document.querySelector(".block1").click(); 
+// Gets day and hours of day
+var date = new Date();
+var hours = date.getHours();
+document.getElementById("currentDay").innerHTML = date;
 
-console.log(click)
+// Each day timeblock
+var blocks = [
+    block1 = document.querySelector(".block1"),
+    block2 = document.querySelector(".block2"),
+    block3 = document.querySelector(".block3"),
+    block4 = document.querySelector(".block4"),
+    block5 = document.querySelector(".block5"),
+    block6 = document.querySelector(".block6"),
+    block7 = document.querySelector(".block7"),
+    block8 = document.querySelector(".block8")
+]
+// sets styling depending on past, current or future.
+function whatTime(hour, block) {
+    if (hours === hour) {
+        block.style.backgroundColor = "green";
+    } else if (hours < hour) {
+        block.style.backgroundColor = "red";
+    } else if (hours > hour) {
+        block.style.backgroundColor = "grey";
+    }
+}
+//for loop for each block
+for (var i = 0; i < blocks.length; i++) {
+    var hourToCheck = i + 9
+    whatTime(hourToCheck, blocks[i])
+    console.log(blocks[i])
+}
